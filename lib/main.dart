188d3414ffb9d26f5362e7cf18eb55ecc631b56d
Priya Risky Qurniawan <80609729+Priyasan181@users.noreply.github.com>
2022-04-15 19:20:59 +0700

    daftar judul lagu

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/screens/Home.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const home());
 }
 
 class MyApp extends StatelessWidget {
