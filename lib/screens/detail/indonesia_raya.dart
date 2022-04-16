43451695f06ec07a109a41639e0fe652cb4bb25d
Nafis Handoko <naffisioner@gmail.com>
2022-04-16 10:13:36 +0700

    nambah teks

--- lib/screens/detail/indonesia_raya.dart
+++ lib/screens/detail/indonesia_raya.dart
@@ -7,7 +7,37 @@ class IndonesiaRaya extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-        child: Text(''),
+        child: Text('''Indonesia tanah airku
+Tanah tumpah darahku
+Disanalah aku berdiri
+Jadi pandu ibuku
+Indonesia kebangsaanku
+Bangsa dan Tanah Airku
+Marilah kita berseru
+Indonesia bersatu
+
+Hiduplah tanahku
+Hiduplah negriku
+Bangsaku Rakyatku semuanya
+Bangunlah jiwanya
+Bangunlah badannya
+Untuk Indonesia Raya
+
+Indonesia Raya
+Merdeka Merdeka
+Tanahku negriku yang kucinta
+
+Indonesia Raya
+Merdeka Merdeka
+Hiduplah Indonesia Raya
+
+Indonesia Raya
+Merdeka Merdeka
+Tanahku negriku yang kucinta
+
+Indonesia Raya
+Merdeka Merdeka
+Hiduplah Indonesia Raya'''),
       ),
     );
   }
